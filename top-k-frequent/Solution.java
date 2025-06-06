d436babd4bd48c2576d510168731a355ef0ba230
rahulsaamanth <rahulsaamanth@yahoo.com>
2025-06-06 10:15:21 +0530

    add: one more solution for top-k-frequnt > Min Heap usign ProirityQueue

--- top-k-frequent/Solution.java
+++ top-k-frequent/Solution.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
+    public int[] topKFrequent_sorting(int[] nums, int k) {
         Map<Integer, Integer> counts = new HashMap<>();
 
         for (int num : nums) {
@@ -24,11 +24,35 @@ class Solution {
         return result;
     }
 
+    public int[] topKFrequent_minheap(int[] nums, int k) {
+        Map<Integer, Integer> counts = new HashMap<>();
+
+        for (int num : nums) {
+            counts.put(num, counts.getOrDefault(num, 0) + 1);
+        }
+
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+
+        for (Map.Entry<Integer, Integer> entry : counts.entrySet()) {
+            heap.offer(new int[] { entry.getValue(), entry.getKey() });
+            if (heap.size() > k) {
+                heap.poll();
+            }
+        }
+
+        int[] result = new int[k];
+
+        for (int i = 0; i < k; i++) {
+            result[i] = heap.poll()[1];
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
         int[] nums = { 1, 1, 1, 2, 2, 3 };
         int k = 2;
-        int[] result = solution.topKFrequent(nums, k);
+        int[] result = solution.topKFrequent_minheap(nums, k);
         System.out.println(Arrays.toString(result));
     }
 }
